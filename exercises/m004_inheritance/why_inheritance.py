7f261f5d311846d4b65e2f35f8b80272fcf85ed9
NhatNM <123147485+NguyenNhat4@users.noreply.github.com>
2025-10-12 09:51:10 +0700

    a

--- exercises/m004_inheritance/why_inheritance.py
+++ exercises/m004_inheritance/why_inheritance.py
@@ -1,34 +1,90 @@
-# Code không có inheritance - lặp lại nhiều các đối tượng có cùng thuộc tính phương thức giống nhau 
-class Dog:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+# Code không có inheritance - lặp lại nhiều
+# class Dog:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
     
-    def action(self):
-        print(f"{self.name} is barking")
+#     def eat(self):
+#         print(f"{self.name} is eating")
     
-    def sound(self):
-        print("Woof!")
+#     def bark(self):
+#         print("Woof!")
 
-class Cat:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+# class Cat:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
     
-    def action(self):
-        print(f"{self.name} is spleeping")
+#     def eat(self):
+#         print(f"{self.name} is eating")
     
-    def sound(self):
-        print("Meow!")
+#     def meow(self):
+#         print("Meow!")
         
+        
+        
+        ############################ Inheritance ############################
+        
+# class Animal:  # Parent/Base class
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+    
+#     def eat(self):
+#         print(f"{self.name} is eating")
 
-class Duck:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-    
-    def action(self):
-        print(f"{self.name} is swimming")
-    
-    def sound(self):
-        print("Quack!")
+
+
+# class Dog(Animal):  # Child class
+#     def bark(self):
+#         print("Woof!")
+
+# class Cat(Animal):
+#     def meow(self):
+#         print("Meow!")
+        
+# class Duck(Animal):
+#     def quack(self):
+#         print("Quack!")
+        
+        
+# c = Cat("Kitty", 3)
+# d = Dog("Buddy", 5)
+# c.eat()
+# d.eat()
+
+
+
+
+
+        ############################ thêm attributes? ############################
+# class Animal:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#         print(f"Animal {name} created!")
+
+# class Dog(Animal):
+#     def __init__(self, name, age, breed):
+#         super().__init__(name, age)  # Gọi __init__ của cha
+#         self.breed = breed
+
+# buddy = Dog("Buddy", 3, "Golden Retriever")
+
+
+# print(buddy.breed)
+# tại sao cần super()?
+
+
+
+
+################### 
+
+# Bạn code, học sinh suggest
+# class Vehicle:
+#     pass
+
+# class Car(Vehicle):
+#     pass
+
+
