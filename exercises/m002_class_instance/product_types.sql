002cf8f4b249e18a711010ba18bc3b2f8eaec7e0
Đình Quân Võ <vodinhquan2714@gmail.com>
2025-11-15 16:22:32 +0100

    a

--- exercises/m002_class_instance/product_types.sql
+++ exercises/m002_class_instance/product_types.sql
@@ -1,3 +1,17 @@
+CREATE TABLE product_types (
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE products (
+    id        SERIAL PRIMARY KEY,
+    name      VARCHAR(100) NOT NULL,
+    price     INTEGER NOT NULL,
+    type_id   INTEGER NOT NULL,
+    FOREIGN KEY (type_id) REFERENCES product_types(id)
+);
+
+
 INSERT INTO products_type (name)
 VALUES
     ('VEGETABLES'),
@@ -6,7 +20,4 @@ VALUES
     ('DAIRY_EGGS'),
     ('MEAT_SEAFOOD');
 
-INSERT INTO products_type (name)
-VALUES
-    ('');
    
