767fd77f09dc296bb5d525d76872537bcad6ed40
NhatNM <123147485+NguyenNhat4@users.noreply.github.com>
2025-11-20 23:17:06 +0700

    add call_llm.py for product classification using Google GenAI

---
+++ exercises/m002_class_instance/call_llm.py
@@ -0,0 +1,43 @@
+from google import genai
+
+client = genai.Client(api_key="AIzaSyDcMfVWbRbt-dYkQpCQ2DmQXVue_iXlhdk")
+
+def call_llm(prompt):
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents=prompt
+    )
+    return response.text
+
+
+def build_prompt(text):
+    return f"""
+    
+    phân loại các sản phẩm trong List sau: 
+    {text}
+    này thành một danh sách tương ứng , thuộc các loại như sau:
+    - VEGETABLES
+    - FRUITS
+    - GRAINS_LEGUMES
+    - DAIRY_EGGS
+    - MEAT_SEAFOOD
+    Example:
+    ```yaml
+    types: [MEAT_SEAFOOD,MEAT_SEAFOODVEGETABLES,GRAINS_LEGUMES,GRAINS_LEGUMES ]
+    ```
+    
+    
+    trả về chinh xac câu trúc YML như trên:
+    """
+import yaml  
+prompt = ""
+with open('./orderitems.txt','r') as f:
+    prompt = f.readlines()
+prompt = build_prompt(prompt)
+
+
+
+yaml_str = call_llm(prompt).split("```yaml")[1].split("```")[0].strip()
+result = yaml.safe_load(yaml_str)
+
+print(result)
