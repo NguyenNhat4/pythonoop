b2c0b75454182b7ca063d4ac86965aff03c3d00c
Đình Quân Võ <vodinhquan2714@gmail.com>
2025-11-22 14:55:23 +0100

    a

--- exercises/m002_class_instance/crud.py
+++ exercises/m002_class_instance/crud.py
@@ -22,32 +22,28 @@ class Product(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     price: Mapped[int] = mapped_column(Integer, nullable=False)
-    type_of_products: Mapped[List["ProductType"]] = relationship(lazy = "selectin")
+    type_id: Mapped[int] = mapped_column(ForeignKey("product_types.id"))
+    product_type: Mapped["ProductType"] = relationship(back_populates="products")
     extend_existing =True
     def __repr__(self) ->str: 
         return f"Product(id={self.id!r}) ,  name={self.name!r}, price={self.price!r})"
-    def to_dict(self) -> Dict[str, int | str]:
-        return {"name": self.name, "price": int(self.price)}
-
 
 class ProductType(Base):
     __tablename__ = "product_types"
     __table_args__ = {"extend_existing":True }
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
+    products: Mapped[List["Product"]] = relationship(
+    back_populates="product_type",  
+    lazy="selectin",  
+    cascade="all, delete-orphan"
+    
+    )
     extend_existing =True
     def __repr__(self) ->str: 
         return f"Product(id={self.id!r}) ,  name={self.name!r})"
     def to_dict(self) -> Dict[str, int | str]:
         return {"name": self.name, "id": self.id}
-    products_id: Mapped[int] = mapped_column(ForeignKey("products_id"))
-
-def create_product(name: str, price: int):
-    with Session(engine) as s:
-        obj  = Product(name = name, price=price) 
-        s.add(obj)
-        s.flush()
-        s.commit()
 
 
 def get_list_products():
