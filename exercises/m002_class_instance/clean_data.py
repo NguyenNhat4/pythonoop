b3a26798b4e8b9b7a5c70d42d101183f56464a3c
Đình Quân Võ <vodinhquan2714@gmail.com>
2025-11-27 15:41:55 +0100

    a

--- exercises/m002_class_instance/clean_data.py
+++ exercises/m002_class_instance/clean_data.py
@@ -1,40 +1,49 @@
 from typing import List
 import os 
 
-
-
-FILE_PATH : str = '' 
+from config import FILE_PATH
 
 
  
 def convert_string_to_list(one_line: str , seperate_by: str = None) ->List[str]: 
-    pass
-#     """
-    
-    
-#     """
-    
-#     one_line = one_line.split()
-#     clean_row = []
-#     name = ""
-#     for element in one_line:
-#         if element.isnumeric():
-#             clean_row.append(int(element))
-#         else:
-#             name = name + " "  + element
-            
-#     name = name.strip()
-#     clean_row.insert(1, name)
-#     return clean_row
+    """
+    Chuyển dòng từ file text thành 1 list của string.
+    Example:
+    Input:
+    one_line = 'FP001,Gao thom ST25,18000,120' , seperate_by = ','
+    Output:
+    ['FP001','Gao thom ST25','18000','120']
 
+    
+    """
+    
+    
+    # clean_row = []
+    # name = ""
+    # for element in one_line:
+    #     if element.isnumeric():
+    #         clean_row.append(int(element))
+    #     else:
+    #         name = name + " "  + element
+            
+    # name = name.strip()
+    # clean_row.insert(1, name)
+    # return clean_row
+    return [x.strip() for x in one_row.split(seperate_by)]
 
 
 if __name__ == '__main__':
     # convert_string_to_list(FILE_PATH)
     if os.path.exists(FILE_PATH):
-        with open(FILE_PATH,r) as f :
-            data_rows = 
-            
+        with open(FILE_PATH,'r') as f :
+            data_rows = f.readlines()
+        one_row = data_rows[2]
+        print(convert_string_to_list(one_row,','))
+        clean_list = []
+        for i in data_rows:
+            if i
+            print(i)     
+        # print(one_row)
         # loop through each row and turn it into a list of string.  
         
     else: 
