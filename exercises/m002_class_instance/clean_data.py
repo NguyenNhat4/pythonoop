e0e8055c9da41fbe1dab14148848574c5c73b5f8
NhatNM <123147485+NguyenNhat4@users.noreply.github.com>
2025-11-27 20:45:29 +0700

    a

--- exercises/m002_class_instance/clean_data.py
+++ exercises/m002_class_instance/clean_data.py
@@ -1,31 +1,41 @@
 from typing import List
+import os 
+
+
 
 FILE_PATH = 
 
 
  
 def convert_string_to_list(one_line: str ) ->List[str]: 
-    """
+    pass
+#     """
     
     
-    """
+#     """
     
-    one_line = one_line.split()
-    clean_row = []
-    name = ""
-    for element in one_line:
-        if element.isnumeric():
-            clean_row.append(int(element))
-        else:
-            name = name + " "  + element
+#     one_line = one_line.split()
+#     clean_row = []
+#     name = ""
+#     for element in one_line:
+#         if element.isnumeric():
+#             clean_row.append(int(element))
+#         else:
+#             name = name + " "  + element
             
-    name = name.strip()
-    clean_row.insert(1, name)
-    return clean_row
+#     name = name.strip()
+#     clean_row.insert(1, name)
+#     return clean_row
 
 
 
 if __name__ == '__main__':
-    convert_string_to_list(FILE_PATH)
-    
-    
+    # convert_string_to_list(FILE_PATH)
+    if os.path.exists(FILE_PATH):
+        with open(FILE_PATH,r) as f :
+            data_rows = 
+            
+        # loop through each row and turn it into a list of string.  
+        
+    else: 
+        print("file dont exist")
